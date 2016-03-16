4494c35385a2c3677113ff3e914a46b3b397a44c
An Phi <aphi@skidmore.edu>
2016-03-15 22:04:59 -0400

    Loai bo databse

--- toDo/views.py
+++ toDo/views.py
@@ -5,3 +5,6 @@ from .models import toDoItem
 def post_list(request):
     list = toDoItem.objects.all()
     return render(request,'toDo/post_list.html',{'list':list})
+
+def post_edit(request):
+    return render(request,'toDo/post_edit.html')
